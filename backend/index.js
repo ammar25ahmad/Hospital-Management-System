b3ff17562c7bcf78f8cc91b26eb708aabd58f698
ammar25ahmad <a25ammar127@gmail.com>
2025-06-01 21:42:01 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const cors = require('cors')  //? For Cross Origin Communications means to send & receive data from backend to frontend
+require('dotenv').config()    //? Package for importing variables from .env file
 const PORT = process.env.PORT
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//? PORT LISTENING
 
 app.listen(PORT, () => {
     console.log("Server Running on PORT 3000")
