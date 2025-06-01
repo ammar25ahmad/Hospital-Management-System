b21fb12546afc56bab789e4ca4db6c869de740f8
ammar25ahmad <a25ammar127@gmail.com>
2025-06-01 21:53:37 +0500

    Second Commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
+// ? Require Package or Modules
+
 const express = require('express')
 const app = express()
-require('dotenv').config()
+const bodyParser = require('body-parser')
 const cors = require('cors')  //? For Cross Origin Communications means to send & receive data from backend to frontend
 require('dotenv').config()    //? Package for importing variables from .env file
-const PORT = process.env.PORT
+const PORT = process.env.PORT  //? PORT Variable from .env file
 
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const corsOption = {
+    origin: ['http://localhost:5173']
+}
+app.use(cors(corsOption))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
