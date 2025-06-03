df189f11def228e53229169ef2247ba0e1a0a323
Muhammed Taha <152517741+tahabeyop@users.noreply.github.com>
2025-06-03 16:09:53 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Form from './Form'
-// import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Form from "./Form";
+// import App from "./App.jsx";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-     <Form />
-  </StrictMode>,
-)
+    {/* <Form /> */}
+  </StrictMode>
+);
