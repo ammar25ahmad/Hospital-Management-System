a27f9e307e344d320fd4dcc0c1ec016e8d1cbb54
Muhammed Taha <152517741+tahabeyop@users.noreply.github.com>
2025-06-04 16:08:33 +0500

    hello
    helo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 function Home() {
-    return(
-        <>
-            <h1>This is the Home page . You have to work on Admin Panel👇</h1>
-            <Link className='border border-2 border-green-400 px-2 py-1 m-2' to="/admin">Admin Panel</Link>
-        </>
-    )
+  return (
+    <>
+      <h1>This is the Home page . You have to work on Admin Panel👇</h1>
+      <Link
+        className="border border-2 border-green-400 px-2 py-1 m-2"
+        to="/admin"
+      >
+        Admin Panel
+      </Link>
+    </>
+  );
 }
 
 export default Home;
